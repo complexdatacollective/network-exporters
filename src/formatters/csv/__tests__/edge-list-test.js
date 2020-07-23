c679bd651bdbdfe1e29b47a093ad71aba6b0bc5c
Joshua Melville <joshua.melville@northwestern.edu>
2020-07-23 13:33:50 +0100

    edge list test

--- src/formatters/csv/__tests__/edge-list-test.js
+++ src/formatters/csv/__tests__/edge-list-test.js
@@ -1,18 +1,45 @@
 /* eslint-env jest */
 import { makeWriteableStream } from '../../../../config/setupTestEnv';
+import { entityPrimaryKeyProperty, edgeSourceProperty, edgeTargetProperty, entityAttributesProperty, exportIDProperty, egoProperty, ncUUIDProperty, ncSourceUUID, ncTargetUUID } from '../../../utils/reservedAttributes';
 import EdgeListFormatter, { asEdgeList, toCSVStream } from '../edge-list';
 
-const listFromEdges = (edges, directed) => asEdgeList({ edges }, directed);
+const mockCodebook = {};
+
+const mockExportOptions = {
+  exportGraphML: true,
+  exportCSV: true,
+  globalOptions: {
+    unifyNetworks: false,
+    useDirectedEdges: false,
+    useScreenLayoutCoordinates: true,
+    screenLayoutHeight: 1080,
+    screenLayoutWidth: 1920,
+  },
+};
+
+const nodes = [
+  { [entityPrimaryKeyProperty]: 1 },
+  { [entityPrimaryKeyProperty]: 2 },
+  { [entityPrimaryKeyProperty]: 3 },
+];
+
+const listFromEdges = (edges, directed = false) => asEdgeList({ edges, nodes }, mockCodebook, {
+  ...mockExportOptions,
+  globalOptions: {
+    ...mockExportOptions.globalOptions,
+    useDirectedEdges: directed,
+  },
+});
 
 describe('asEdgeList', () => {
   it('takes a network as input', () => {
     const network = {
       nodes: [],
-      edges: [{ _uid: 456, from: 'nodeA', to: 'nodeB', type: 'type', attributes: {} }],
-      ego: { _uid: 123 },
+      edges: [{ [entityPrimaryKeyProperty]: 456, [edgeSourceProperty]: 'nodeA', [edgeTargetProperty]: 'nodeB', type: 'type', [entityAttributesProperty]: {} }],
+      ego: { [entityPrimaryKeyProperty]: 123 },
     };
-    expect(asEdgeList(network)[0]).toEqual(
-      { _uid: 456, to: 'nodeB', from: 'nodeA', type: 'type', attributes: {} },
+    expect(asEdgeList(network, mockCodebook, mockExportOptions)[0]).toEqual(
+      { [entityPrimaryKeyProperty]: 456, [edgeTargetProperty]: 'nodeB', [edgeSourceProperty]: 'nodeA', type: 'type', [entityAttributesProperty]: {} },
     );
   });
 
@@ -21,21 +48,20 @@ describe('asEdgeList', () => {
   });
 
   it('represents a single undirected edge', () => {
-    expect(listFromEdges([{ from: 1, to: 2 }])).toEqual([
-      { from: 1, to: 2, attributes: {} },
-      { from: 2, to: 1, attributes: {} },
+    expect(listFromEdges([{ [edgeSourceProperty]: 1, [edgeTargetProperty]: 2 }])).toEqual([
+      { [edgeSourceProperty]: 1, [edgeTargetProperty]: 2, [entityAttributesProperty]: {} },
     ]);
   });
 
   it('represents a single directed edge', () => {
-    expect(listFromEdges([{ from: 1, to: 2 }], true)).toEqual([
-      { from: 1, to: 2, attributes: {} },
+    expect(listFromEdges([{ [edgeSourceProperty]: 1, [edgeTargetProperty]: 2 }], true)).toEqual([
+      { [edgeSourceProperty]: 1, [edgeTargetProperty]: 2, [entityAttributesProperty]: {} },
     ]);
   });
 
   it('include egoID', () => {
-    expect(listFromEdges([{ _egoID: 123, from: 1, to: 2 }], true)).toEqual([
-      { _egoID: 123, from: 1, to: 2, attributes: {} },
+    expect(listFromEdges([{ _egoID: 123, [edgeSourceProperty]: 1, [edgeTargetProperty]: 2 }], true)).toEqual([
+      { _egoID: 123, [edgeSourceProperty]: 1, [edgeTargetProperty]: 2, [entityAttributesProperty]: {} },
     ]);
   });
 });
@@ -47,49 +73,41 @@ describe('toCSVStream', () => {
     writable = makeWriteableStream();
   });
 
-  it('Writes a simple csv', async () => {
-    const list = listFromEdges([{ _uid: 123, type: 'type', from: 1, to: 2 }]);
-    toCSVStream(list, writable);
-    const csv = await writable.asString();
-    expect(csv).toEqual('networkCanvasEdgeID,networkCanvasEdgeType,networkCanvasSource,networkCanvasTarget\r\n291,type,1,2\r\n291,type,2,1\r\n');
-  });
-
-  it('Writes multiple edges', async () => {
-    const list = listFromEdges([{ from: 1, to: 2 }, { from: 1, to: 3 }]);
-    toCSVStream(list, writable);
-    const csv = await writable.asString();
-    expect(csv).toEqual('networkCanvasEdgeID,networkCanvasEdgeType,networkCanvasSource,networkCanvasTarget\r\n,,1,2\r\n,,2,1\r\n,,1,3\r\n,,3,1\r\n');
-  });
-
-  it('Writes a csv for directed edges', async () => {
-    const list = listFromEdges([{ from: 1, to: 2 }, { from: 1, to: 3 }], true);
-    toCSVStream(list, writable);
-    const csv = await writable.asString();
-    expect(csv).toEqual('networkCanvasEdgeID,networkCanvasEdgeType,networkCanvasSource,networkCanvasTarget\r\n,,1,2\r\n,,1,3\r\n');
-  });
-
-  it('Writes a csv for directed edges (inverse)', async () => {
-    const list = listFromEdges([{ from: 1, to: 2 }, { from: 3, to: 1 }], true);
-    toCSVStream(list, writable);
-    const csv = await writable.asString();
-    expect(csv).toEqual('networkCanvasEdgeID,networkCanvasEdgeType,networkCanvasSource,networkCanvasTarget\r\n,,1,2\r\n,,3,1\r\n');
-  });
-
-  it('Writes a csv with egoID', async () => {
-    const list = listFromEdges(
-      [{ _egoID: 123, from: 1, to: 2 }, { _egoID: 456, from: 3, to: 1 }],
-      true,
-    );
-    toCSVStream(list, writable, true);
-    const csv = await writable.asString();
-    expect(csv).toEqual('networkCanvasEgoID,networkCanvasEdgeID,networkCanvasEdgeType,networkCanvasSource,networkCanvasTarget\r\n291,,,1,2\r\n1110,,,3,1\r\n');
-  });
-
   it('Writes a csv with attributes', async () => {
-    const list = listFromEdges([{ from: 1, to: 2, attributes: { a: 1 } }], true);
+    const list = listFromEdges([
+      {
+        [entityPrimaryKeyProperty]: 123,
+        [egoProperty]: 456,
+        [exportIDProperty]: 1,
+        [ncSourceUUID]: 1,
+        [ncTargetUUID]: 2,
+        [edgeSourceProperty]: 1,
+        [edgeTargetProperty]: 2,
+        [entityAttributesProperty]: {
+          a: 1,
+        },
+      },
+    ]);
     toCSVStream(list, writable);
     const csv = await writable.asString();
-    expect(csv).toEqual('networkCanvasEdgeID,networkCanvasEdgeType,networkCanvasSource,networkCanvasTarget,a\r\n,,1,2,1\r\n');
+    const result = [
+      exportIDProperty,
+      edgeSourceProperty,
+      edgeTargetProperty,
+      egoProperty,
+      ncUUIDProperty,
+      ncSourceUUID,
+      ncTargetUUID,
+      'a\r\n1',
+      1,
+      2,
+      456,
+      123,
+      1,
+      2,
+      '1\r\n',
+    ].join(',');
+    expect(csv).toEqual(result);
   });
 });
 
@@ -101,7 +119,7 @@ describe('EdgeListFormatter', () => {
   });
 
   it('writeToStream returns an abort controller', () => {
-    const formatter = new EdgeListFormatter({});
+    const formatter = new EdgeListFormatter({}, mockCodebook, mockExportOptions);
     const controller = formatter.writeToStream(writable);
     expect(controller.abort).toBeInstanceOf(Function);
   });
