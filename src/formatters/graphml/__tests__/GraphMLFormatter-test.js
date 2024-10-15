bd964415b53bb9dcdcc21d300c5e3655afdec68e
Joshua Melville <joshua.melville@northwestern.edu>
2024-10-15 13:56:10 +0200

    add test for GraphML string export; change GraphML implementation of writeToString to match CSV

--- src/formatters/graphml/__tests__/GraphMLFormatter-test.js
+++ src/formatters/graphml/__tests__/GraphMLFormatter-test.js
@@ -3,6 +3,7 @@
 import { Writable } from 'stream';
 import { mockNetwork, mockCodebook } from '../../../../config/mockObjects';
 import GraphMLFormatter from '../GraphMLFormatter';
+import { DOMParser } from '@xmldom/xmldom';
 
 const makeWriteableStream = () => {
   const chunks = [];
@@ -21,6 +22,38 @@ const makeWriteableStream = () => {
 
   return writable;
 };
+
+describe('GraphMLFormatter writeToString', () => {
+  let network;
+  let codebook;
+  let exportOptions;
+
+  beforeEach(() => {
+    network = mockNetwork;
+    codebook = mockCodebook;
+    exportOptions = {
+      exportGraphML: true,
+      exportCSV: false,
+      globalOptions: {
+        resequenceIDs: false,
+        unifyNetworks: false,
+        useDirectedEdges: false,
+      },
+    };
+  });
+
+  it('produces valid XML', () => {
+    const formatter = new GraphMLFormatter(network, codebook, exportOptions);
+    const xml = formatter.writeToString();
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xml, 'text/xml');
+
+    expect(xmlDoc.documentElement.nodeName).toBe('graphml');
+    expect(xmlDoc.documentElement.getElementsByTagName('graph').length).toBe(1);
+    expect(xmlDoc.documentElement.getElementsByTagName('node').length).toBe(4);
+    expect(xmlDoc.documentElement.getElementsByTagName('edge').length).toBe(1);
+  });
+});
 
 describe('GraphMLFormatter writeToStream', () => {
   let network;
